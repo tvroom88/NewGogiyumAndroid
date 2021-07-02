807a1b94ab557e9ba77101147eaa694f849a187e
JaehongLee <tvroom88@naver.com>
2021-07-01 22:28:59 -0700

    changed recommand and add menu and action bar

--- app/src/main/java/com/newgogiyumandroid/MainActivity.kt
+++ app/src/main/java/com/newgogiyumandroid/MainActivity.kt
@@ -3,34 +3,41 @@ package com.newgogiyumandroid
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.newgogiyumandroid.JsonParsingLists.FoodList
 import com.newgogiyumandroid.RecyclerView.CustomAdapter
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import okhttp3.*
 import org.json.JSONArray
 import java.io.IOException
-import java.net.URL
 
-// SplashActivity 추가도 고려해볼만 하다
-//
 
 class MainActivity : AppCompatActivity() {
 
-    var data: MutableList<FoodList> = mutableListOf()
+    companion object {
+        private const val MENU_URL = "https://gogiyum.com/api/menu"
+        var data: MutableList<FoodList> = mutableListOf()
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setSupportActionBar(toolbar) //커스텀한 toolbar를 액션바로 사용
+        supportActionBar?.setDisplayShowTitleEnabled(false) //액션바에 표시되는 제목의 표시유무를 설정합니다. false로 해야 custom한 툴바의 이름이 화면에 보이게 됩니다.
+
         if (checkInternetConnection()) {
-            readData();
+            readData()
         } else {
             progressImg.visibility = View.GONE
         }
@@ -38,7 +45,7 @@ class MainActivity : AppCompatActivity() {
 
     fun readData() {
         val client = OkHttpClient.Builder().build()
-        val req = Request.Builder().url("https://gogiyum.com/api/menu").build()
+        val req = Request.Builder().url(MENU_URL).build()
         client.newCall(req).enqueue(object: Callback {
             override fun onFailure(call: Call, e: IOException) {
                 // failure
@@ -47,7 +54,7 @@ class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call, response: Response) {
                 val launch = CoroutineScope(Dispatchers.Main).launch {
 
-                    val body = response.body!!.string()
+                    val body = response.body?.string()
                     val jsonArray = JSONArray(body)
 
                     for (i in 0 until jsonArray.length()) {
@@ -86,4 +93,29 @@ class MainActivity : AppCompatActivity() {
 
         return false
     }
+
+    //액션버튼 메뉴 액션바에 집어 넣기
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    //액션버튼 클릭 했을 때
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.language -> {
+                item.setIcon(R.drawable.en1)
+                item.setTitle("Language : English")
+                Toast.makeText(applicationContext, "Language : English", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.bookmark ->{
+                Toast.makeText(applicationContext, "click on bookmark", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
